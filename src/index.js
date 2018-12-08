aeea2897f2d001701172a600edaee30b8039e913
Jaid <jaid.jsx@gmail.com>
2018-12-08 05:54:02 +0100

    Improved config loading

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
-import cosmiconfig from "cosmiconfig"
-import ExConfig from "ex-config"
-import readPkgUp from "read-pkg-up"
-import humanizeList from "humanize-list"
 import generatePackage from "./generatePackage"
-import searchPlaces from "./searchPlaces?aot"
+import loadConfig from "lib/loadConfig"
+import {defaults} from "lodash"
 
-const name = "publishimo"
-
-export default () => {
-  const exConfig = new ExConfig
-  const explorer = cosmiconfig(name, {searchPlaces})
-
-  const rawPackage = readPkgUp.sync()
-  if (!rawPackage?.pkg) {
-    throw Error("Could not find package.json")
-  }
-
-  const rawConfig = explorer.searchSync()
-  if (!rawConfig?.config) {
-    const humanizedSearchPlaces = humanizeList(searchPlaces.map((place, i) => i ? place : `package.json ("${name}" key)`))
-    throw Error(`Could not find a valid ${name} configuration. Looked for files named: ${humanizedSearchPlaces}`)
-  }
-
-  const rawExConfig = exConfig.load(rawConfig.config)
-  console.log(rawConfig)
-  console.log(rawExConfig)
-  return
-
-  const generatedPackage = generatePackage(rawPackage.pkg, rawConfig.config)
+export default options => {
+  options = defaults(options, {
+    cwd: process.cwd(),
+    config: null
+  })
+  const {pkg, config} = loadConfig(options)
+  const generatedPackage = generatePackage(pkg, config)
   console.log(generatedPackage)
 }
---
+++ src/lib/loadConfig.js
@@ -0,0 +1,56 @@
+import cosmiconfig from "cosmiconfig"
+import ExConfig from "ex-config"
+import readPkgUp from "read-pkg-up"
+import {isString} from "lodash"
+
+const name = "publishimo"
+
+// cwd
+// config
+// package
+export default options => {
+  const pkg = readPkgUp.sync({cwd: options.cwd})?.pkg
+  if (!pkg) {
+    throw Error("Could not find package.json")
+  }
+
+  const exConfig = new ExConfig
+
+  const searchPlaces = [
+    "package.json",
+    `.${name}rc`,
+    `.${name}rc.js`,
+    `${name}.config.js`,
+    `${name}.json`,
+    `.${name}.json`,
+    `${name}.yaml`,
+    `${name}.yml`,
+    `.${name}.yaml`,
+    `.${name}.yml`
+  ]
+  const explorer = cosmiconfig(name, {
+    searchPlaces,
+    cwd: options.cwd
+  })
+
+  let config
+  if (options.config) {
+    config = explorer.loadSync(options.config)?.config
+  } else if (pkg[name] |> isString) {
+    config = explorer.loadSync(pkg[name])?.config
+  } else {
+    config = explorer.searchSync()?.config
+  }
+
+  if (!config) {
+    const humanizedSearchPlaces = searchPlaces.map((place, i) => i ? place : `package.json ("${name}" key)`).join(", ")
+    throw Error(`Could not find a valid ${name} configuration. Looked for files named: ${humanizedSearchPlaces}`)
+  }
+
+  const completeConfig = exConfig.load(config)
+
+  return {
+    pkg,
+    config: completeConfig
+  }
+}
