7bb357ce063e31670004a78dcbf462ce837cb35a
Jaid <jaid.jsx@gmail.com>
2018-12-08 02:51:30 +0100

    Improved author and contributors field

--- src/lib/cleanAuthor.js
+++ src/lib/cleanAuthor.js
@@ -1,7 +1,14 @@
 import {pick, trim, mapValues} from "lodash"
 import objClean from "obj-clean"
+import normalizeUrl from "normalize-url"
 
-export default x => x
+export default x => {
+  x = x
   |> (_ => pick(_, ["name", "email", "url"]))
   |> objClean
   |> (_ => mapValues(_, trim))
+  if (x.url) {
+    x.url = x.url |> normalizeUrl
+  }
+  return x
+}
