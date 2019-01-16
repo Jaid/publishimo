fc3e4060a94cca2373f3b4889e9759a129d4729a
Jaid <jaid.jsx@gmail.com>
2019-01-16 05:49:13 +0100

    Fixed paths in webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,11 +3,11 @@ import path from "path"
 import webpackConfigNode from "webpack-config-node"
 
 const isDevelopment = process.env.NODE_ENV !== "production"
-const fromPackage = directive => path.resolve(__dirname, "..", directive)
+const fromPackage = directive => path.resolve(__dirname, directive)
 
 export default webpackConfigNode({
   isDevelopment,
-  packageRoot: path.resolve(__dirname, ".."),
+  packageRoot: __dirname,
   extra: {
     resolve: {
       alias: {
