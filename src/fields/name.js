68f399b3e708362646fa0e341f4a904831b05d61
Jaid <jaid.jsx@gmail.com>
2019-01-19 01:42:04 +0100

    Improved name field

--- src/fields/name.js
+++ src/fields/name.js
@@ -2,14 +2,15 @@ import path from "path"
 
 import cleanString from "lib/cleanString"
 
-export const prepare = ({getAny}) => getAny()
-
-export const apply = ({myMeta, sourcePkgLocation}) => {
-  if (myMeta) {
-    return myMeta |> cleanString
+export const prepare = ({getAny, sourcePkgLocation}) => {
+  const configValue = getAny()
+  if (configValue) {
+    return configValue |> cleanString
   }
   if (sourcePkgLocation) {
     return sourcePkgLocation |> path.dirname |> path.basename |> cleanString
   }
   return "publishimo-output"
 }
+
+export const applyMeta = x => x
