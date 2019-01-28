c4ba7c08a068142ad7d23069a9728d2c0058c076
Jaid <jaid.jsx@gmail.com>
2019-01-28 19:44:20 +0100

    Omitted main field if it is "index.js"

--- src/fields/main.js
+++ src/fields/main.js
@@ -1,4 +1,9 @@
 import cleanPath from "lib/cleanPath"
 
 export const prepare = ({getAny}) => getAny()
-export const applyMeta = x => x |> cleanPath
+export const applyMeta = path => {
+  if (path.trim() === "index.js") { // "main": "index.js" is not needed, says https://docs.npmjs.com/creating-a-package-json-file#default-values-extracted-from-the-current-directory
+    return
+  }
+  return path |> cleanPath
+}
